ea89ea881be594a413d2fd10be8d0a17956f4ad9
refractiOOn <etozhesotnikow@gmail.com>
2021-12-15 10:01:31 +0200

    added vector of program names, vector of handles

--- AntivirusMonitor.cpp
+++ AntivirusMonitor.cpp
@@ -1,20 +1,40 @@
-// AntivirusMonitor.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
-
 #include <iostream>
+#include <vector>
+#include <sstream>
+#include <mutex>
+#include <windows.h>
+#include <stdio.h>
+#include <psapi.h>
+
+DWORD __stdcall ThreadProc(LPVOID lpParameter)
+{
+	std::wstring bannedProcess = *static_cast<std::wstring*>(lpParameter);
+	std::wcout << bannedProcess << '\n';
+	return 0;
+}
 
 int main()
 {
-    std::cout << "Hello World!\n";
+	std::wstring str;
+	std::getline(std::wcin, str);
+	std::wstringstream stream;
+	stream << str;
+	std::vector<std::wstring> bannedProcesses;
+	while (stream)
+	{
+		std::wstring temp;
+		stream >> temp;
+		bannedProcesses.push_back(temp);
+	}
+
+	std::vector<HANDLE> threads;
+	for (size_t i = 0; i < bannedProcesses.size(); ++i)
+	{
+		threads.push_back(CreateThread(0, 0, ThreadProc, &bannedProcesses[i], 0, 0));
+	}
+
+	if (!threads.empty())
+	{
+		WaitForMultipleObjects(threads.size(), &threads[0], TRUE, INFINITE);
+	}
 }
-
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
-
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
