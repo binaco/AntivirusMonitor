9236e66df952abea532ecdd5f3efdb04fa58ec04
refractiOOn <etozhesotnikow@gmail.com>
2021-12-15 16:23:23 +0200

    added KillProcessByName, multithreading, HandleWrapperClass

--- AntivirusMonitor.cpp
+++ AntivirusMonitor.cpp
@@ -1,15 +1,99 @@
 #include <iostream>
-#include <vector>
 #include <sstream>
+#include <vector>
 #include <mutex>
 #include <windows.h>
 #include <stdio.h>
 #include <psapi.h>
 
+size_t g_killedProcessesCount = 0;
+
+class HandleWrapper
+{
+public:
+    HandleWrapper(HANDLE handle) :
+        m_handle(handle)
+    {
+
+    }
+    ~HandleWrapper()
+    {
+        if (m_handle && m_handle != INVALID_HANDLE_VALUE)
+        {
+            CloseHandle(m_handle);
+        }
+    }
+    HANDLE Get() const
+    {
+        return m_handle;
+    }
+private:
+    HANDLE m_handle = INVALID_HANDLE_VALUE;
+};
+
+bool KillProcessByName(DWORD processID, const std::wstring& name)
+{
+    HandleWrapper process(OpenProcess(PROCESS_ALL_ACCESS, FALSE, processID));
+    if (!process.Get())
+    {
+        return 0;
+    }
+
+    std::wstring processName(MAX_PATH, L'\0');
+    DWORD nameSize = GetModuleBaseName(process.Get(), 0, &processName[0], processName.length());
+    if (!nameSize)
+    {
+        return 0;
+    }
+    processName.resize(nameSize);
+
+    if (processName == name)
+    {
+        if (!TerminateProcess(process.Get(), 1))
+        {
+            return 0;
+        }
+        else
+        {
+            return 1;
+        }
+    }
+
+    return 0;
+}
+
 DWORD __stdcall ThreadProc(LPVOID lpParameter)
 {
 	std::wstring bannedProcess = *static_cast<std::wstring*>(lpParameter);
-	std::wcout << bannedProcess << '\n';
+    std::mutex lock;
+    while (g_killedProcessesCount < 5)
+    {
+        //lock.lock();
+        Sleep(1000);
+        DWORD processes[1024], bytesReturned;
+        if (!EnumProcesses(processes, sizeof(processes), &bytesReturned))
+        {
+            std::cout << "Could not get processes ids" << std::endl;
+        }
+        DWORD processesAmount = bytesReturned / sizeof(DWORD);
+        bool isKilled = false;
+        for (size_t i = 0; i < processesAmount; ++i)
+        {
+            if (processes[i])
+            {
+                if (KillProcessByName(processes[i], bannedProcess))
+                {
+                    isKilled = true;
+                }
+            }
+        }
+        if (isKilled)
+        {
+            std::cout << "Process terminated successfully" << std::endl;
+            ++g_killedProcessesCount;
+        }
+        //lock.unlock();
+    }
 	return 0;
 }
 
@@ -37,4 +121,12 @@ int main()
 	{
 		WaitForMultipleObjects(threads.size(), &threads[0], TRUE, INFINITE);
 	}
+
+    for (size_t i = 0; i < threads.size(); ++i)
+    {
+        if (threads[i] != INVALID_HANDLE_VALUE)
+        {
+            CloseHandle(threads[i]);
+        }
+    }
 }
